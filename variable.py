67b55e043a07613b9c62accd48328999a2348677
FATHUR RAHMAN ABDULLAH <63894938+fathurrahman0530@users.noreply.github.com>
2021-10-31 14:27:57 +0800

    study variable python

---
+++ variable.py
@@ -0,0 +1,24 @@
+# variable untuk menyimpan data
+
+x = 10
+y = 50
+nilai_panjang = 120000
+
+# pemanggilan pertama
+print("Nilai X = ", x)
+print("Nilai Y = ", y)
+print("Nilai Panjang = ", nilai_panjang)
+
+# penamaan kedua
+nilai_y = 15
+jutaan  = 1250000
+nilaiz  = 12.5 
+
+# pemanggilan kedua
+print("Nilai X = ", x)
+x = 5
+print("Nilai X = ", x)
+
+# assignment indirect
+a = x
+print("Nilai A = ", a)
