9549a90c38a527659d60da72aa4393de2d961d56
FATHUR RAHMAN ABDULLAH <63894938+fathurrahman0530@users.noreply.github.com>
2021-12-06 19:44:15 +0800

    Create main.py

---
+++ lanjutan/main.py
@@ -0,0 +1,11 @@
+# input user
+
+# proses input data default (string)
+data = input("Masukkan username : ")
+
+print("username = ", data, ", type = ", type(data))
+
+# proses input data integer
+data_int = int(input("Masukkan angka : "))
+
+print("angka yang dimasukkan = ", data_int, "type = ", type(data_int))
